30bcd66e9b82444423cbd79bb979d7b07ef4124c
yoann <yoanstudys@gmail.com>
2025-08-31 18:58:15 +0700

    title

--- resources/views/spkrekom/index.blade.php
+++ resources/views/spkrekom/index.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>document</title>
+  <title>Film Recommendation</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 </head>
@@ -380,7 +380,7 @@
                 <div class="card-body d-flex flex-column">
                   <h5 class="mb-3"><i class="bi bi-calendar-event text-danger"></i> Release Year</h5>
                   <select name="year" class="form-select custom-select">
-                    <option value="">Any Year</option>
+                    <option value="">Select Year</option>
                     <option value="2020-2025">2020 - 2025</option>
                     <option value="2015-2019">2015 - 2019</option>
                     <option value="2010-2014">2010 - 2014</option>
