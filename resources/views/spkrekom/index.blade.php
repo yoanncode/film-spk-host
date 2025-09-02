aa132a925c8fcd74ae72c3de84fd6d8d28b581d3
yoann <yoanstudys@gmail.com>
2025-09-02 19:15:01 +0700

    update navbar

--- resources/views/spkrekom/index.blade.php
+++ resources/views/spkrekom/index.blade.php
@@ -295,32 +295,36 @@
 </style>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-color fixed-top">
-        <div class="container">
-          <a class="navbar-brand fs-4 glow-text" href="#">MHSmovie.</a>
-          <button class="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="sidebar offcanvas offcanvas-start bg-dark" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-            <div class="offcanvas-header text-white border-bottom">
-              <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
-              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body d-flex justify-content-betweeb align-items-center">
-              <ul class="navbar-nav justify-content-between">
-                <li class="nav-item mx-2">
-                  <a class="nav-link" href="#home"> Home</a>
-                </li>
-                <li class="nav-item mx-2">
-                  <a class="nav-link" href="#recommendation"> Recommendations</a>
-                </li>
-                <li class="nav-item mx-2">
-                  <a class="nav-link" href="#about"> About</a>
-                </li>
-              </ul>
-            </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-color fixed-top">
+      <div class="container">
+        <a class="navbar-brand fs-4 glow-text" href="#">MHSmovie.</a>
+        <button class="navbar-toggler shadow-none border-0" type="button" 
+                data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" 
+                aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="sidebar offcanvas offcanvas-start bg-dark" tabindex="-1" 
+            id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div class="offcanvas-header text-white border-bottom">
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+            <button type="button" class="btn-close btn-close-white" 
+                    data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body d-flex justify-content-between align-items-center">
+            <ul class="navbar-nav justify-content-between">
+              <li class="nav-item mx-2">
+                <a class="nav-link" href="#home">Home</a>
+              </li>
+              <li class="nav-item mx-2">
+                <a class="nav-link" href="#recommendation">Recommendations</a>
+              </li>
+              <li class="nav-item mx-2">
+                <a class="nav-link" href="#about">About</a>
+              </li>
+            </ul>
           </div>
         </div>
+      </div>
     </nav>
     
     <section id="home" class="hero d-flex align-items-center">
