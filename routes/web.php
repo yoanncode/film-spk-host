58dc07067d44cd47e1af21661e4cc8ac180ae67a
yoann <yoanstudys@gmail.com>
2025-10-27 16:14:06 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FilmController;
 
-Route::get('/recommend', [FilmController::class, 'recommend'])->name('recommend');
+Route::get('/', function () {
+    return redirect('/recommend');
+});
 
+Route::get('/recommend', [FilmController::class, 'recommend'])->name('recommend');
